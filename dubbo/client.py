b65f12ea039b33a2deb2597c55dc578ac4eb391b
林尧 <wangxiaobin@cai-inc.com>
2024-09-03 19:53:35 +0800

    Supplement the missing version and reversion fields, and change the default version to an empty string for the Java version

--- dubbo/client.py
+++ dubbo/client.py
@@ -40,7 +40,7 @@ class DubboClient(object):
     用于实现dubbo调用的客户端
     """
 
-    def __init__(self, interface, version='1.0.0', dubbo_version='2.4.10', zk_register=None, host=None):
+    def __init__(self, interface, version='', dubbo_version='2.4.10', zk_register=None, host=None):
         """
         :param interface: 接口名，例如：com.qianmi.pc.es.api.EsProductQueryProvider
         :param version: 接口的版本号，例如：1.0.0，默认为1.0.0
@@ -269,10 +269,10 @@ class ZkRegister(object):
             'interface': provider_fields['interface'],
             'methods': provider_fields['methods'],
             'pid': get_pid(),
-            'revision': provider_fields['revision'],
+            'revision': provider_fields.get('revision'),
             'side': 'consumer',
             'timestamp': int(time.time() * 1000),
-            'version': provider_fields['version'],
+            'version': provider_fields.get('version'),
         }
 
         params = []
